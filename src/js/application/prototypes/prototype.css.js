f7fbcdaaf8f09f433b930248ea262de1c9938ae6
jiri-kralovec-eal <jiri0042@edu.eal.dk>
2018-11-12 15:06:53 +0100

    Firefox fix for map

--- src/js/application/prototypes/prototype.css.js
+++ src/js/application/prototypes/prototype.css.js
@@ -1,9 +1,6 @@
 Element.prototype.Styles = function(f){
-    for(let key in f) {
-        if(f.hasOwnProperty(key) && this.style.hasOwnProperty(key)){
-            this.style[key] = f[key];
-        }
+    for(let prop in f) {
+        this.style[prop] = f[prop];
     }
-    
     return this;
 }
